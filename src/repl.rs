a22601933f3995c96cc52e04a31360e08e56a4f2
Artem Golovin <artgolovin96@gmail.com>
2021-07-28 00:02:22 -0600

    fix: use match for stdin

--- src/repl.rs
+++ src/repl.rs
@@ -1,23 +1,22 @@
-use crate::lexer;
 use crate::lexer::Lexer;
 use crate::token::Token;
 use std::io;
-use std::io::Write;
-
-const PROMPT: &str = "> ";
 
 pub fn repl() {
     loop {
         let mut input = String::new();
-        io::stdin().read_line(&mut input);
-        if input.ends_with("\n") || input.ends_with("\r\n") {
-            let mut lexer = Lexer::new(input);
-            let mut token = lexer.next_token();
-            while token != Token::Eof {
-                println!("{:?}", token);
-                token = lexer.next_token();
-                io::stdout().flush();
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                if input.ends_with("\n") || input.ends_with("\r\n") {
+                    let mut lexer = Lexer::new(input);
+                    let mut token = lexer.next_token();
+                    while token != Token::Eof {
+                        println!("{:?}", token);
+                        token = lexer.next_token();
+                    }
+                }
             }
+            Err(e) => panic!("{:?}", e),
         }
     }
 }
